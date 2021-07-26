a03298bde138b809c80c4b506b02c6c8a7c50e0d
Zyto_Rex <jacob.wolford@gmail.com>
2021-07-26 18:34:06 +0000

    Added more loops

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from turtle import Turtle, Screen 
-#make turtle and screen
+#make turtle and screen and determine sidelength
 t = Turtle() 
 s = Screen()
+s.bgcolor('green')
+t.color('red')
+#sideLength: int = float(input('Give a side length: '))
+sideLength = s.numinput('Input','Give a number')
 # draw square
-def square():
-  for x in range(4):
-  t.forward(40)
-  t.rt(90)
-  t.forward(40)
 
-y = square()
+for x in range(4):
+  t.forward(sideLength)
+  t.rt(90)   
+  t.forward(sideLength)
+  
+sideNum = 0
+while sideNum <4:
+  t.forward(sideLength*2)
+  t.rt(90)   
+  t.forward(sideLength*2)
+  sideNum+=1
+
+t.right(180)
+while sideNum <8:
+  t.forward(sideLength*2)
+  t.rt(90)   
+  t.forward(sideLength*2)
+  sideNum+=1
+
+for x in range(4):
+  t.forward(sideLength)
+  t.rt(90)   
+  t.forward(sideLength)
