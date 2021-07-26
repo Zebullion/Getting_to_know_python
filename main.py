5efa977cdf3a6e73af8c0c5f3006fdf059fd951a
Zyto_Rex <jacob.wolford@gmail.com>
2021-07-26 17:44:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen 
+#make turtle and screen
+t = Turtle() 
+s = Screen()
+def square():
+  for x in range(4):
+  t.forward(40)
+  t.rt(90)
+  t.forward(40)
+
+y = square()
